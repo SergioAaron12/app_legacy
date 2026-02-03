3965a40ac478b9295ecaf24ed3b9d485b879591b
SergioAaron12 <elsakitowea@gmail.com>
2026-02-03 14:36:32 -0300

    Actualizar proyecto

--- auth_movil/src/main/java/com/ms_auth/auth/dto/TokenDto.java
+++
@@ -1,9 +0,0 @@
-package com.ms_auth.auth.dto;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-
-@Data
-@AllArgsConstructor
-public class TokenDto {
-    private String token;
-}
--- productos/src/main/java/com/ms_productos/productos/repository/CategoriaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ms_productos.productos.repository;
-import com.ms_productos.productos.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
